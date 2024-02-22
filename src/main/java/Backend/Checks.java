acb25159f6581197410d7f22ea0f12977cdf260e
darrenl <darrenl@ZAKZRHB-CLLB24.ballito.local>
2024-02-22 14:14:46 +0200

    I HATE YOU

---
+++ src/main/java/Backend/Checks.java
@@ -0,0 +1,136 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import java.time.LocalDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author darrenl
+ */
+public class Checks {
+    
+    
+    public boolean emailValdity(String email){
+        // The checks required for the email
+        boolean hasASpecial = false;
+        boolean hasADot = false;
+        
+        //The location of the @ Symbol to prevent issues involving emails such as darren.larkens@reddam.house where the email could still be invalid as there could still be
+        //an inadaquate refferance email. Eg darren.larkens@gmail
+        int locationOfSpecial = -1;
+        
+        //Checks if the email is present in the field
+        if(email.length() <= 0){
+            return false;
+        }
+        
+        //Checking email for presence and location of @ symbol
+        for(int i = 0; i < email.length(); i++){
+            char checkingCharacter = email.charAt(i);
+            
+            //Sets the check to true and stores location of @ symbol
+            if(checkingCharacter == '@'){
+                hasASpecial = true;
+                locationOfSpecial = i;
+                break;
+            }
+            
+        }
+        
+        //Final check to completly erradicate any possiblities of infinite loops or indexing errors
+        if(locationOfSpecial > 0){
+            
+            //Loops through the string PAST the point of the @ symbol to ensure a valid email
+            for(int f = locationOfSpecial; f < email.length(); f++){
+                char CheckingCharacter = email.charAt(f);
+                
+                //Checks for the dot
+                if(CheckingCharacter == '.'){
+                    hasADot = true;
+                    break;
+                }
+            }
+        }
+        
+        
+        //Combines both validity fields to ensure the email is valid
+        
+        //Returns true if both are met and false if they are not
+        if(hasASpecial = true && hasADot == true){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    
+    
+    public boolean cardDateValidity(LocalDateTime cardExperationDate){
+        boolean isCorrectCardDate = true;
+        
+        
+        if(cardExperationDate.isAfter(cardExperationDate.plusYears(3))){
+            isCorrectCardDate = false;
+        }
+        
+        return isCorrectCardDate;
+        
+        
+    }
+    
+    public boolean cardNumberCheck(String cardNumber){
+        boolean correctLenght = false;
+        boolean noIllegalCharactersPresent = false;
+        
+        if(cardNumber.length() <= 0){
+            return false;
+        }
+        
+        if(cardNumber.length() == 19){
+            correctLenght = true;
+        }
+        else{
+            return false;
+        }
+        
+        // CHECKS IF ALL CHARACTERS ARE DIGITS
+    }
+    
+    public void printReciept(int priceOfItem, int ISBNofItem){
+        System.out.println("-------------- Recpiept ------------- \nPrice Of Item: R" + priceOfItem +"\nISBN of Item: " + ISBNofItem + "\n\n---------------------- THANK YOU FOR YOUR PURCHASE! --------------------");
+    }
+    
+    public boolean ISBNcheck(int ISBN){
+        boolean validISBN = false;
+        return validISBN;
+    }
+    
+    
+    public boolean lengthCheck(int length,String input){
+        boolean isCorrectLength = true;
+        
+        
+        if(input.length() > length || input.length() < length){
+            isCorrectLength = false;
+        }
+        
+        return isCorrectLength;
+    }
+    
+    public boolean priceRangeCheck(int maxPrice,String input){
+        if(input.length() > maxPrice){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+
+
